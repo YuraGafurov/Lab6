a03d2fd6f8ecb75d3c17c818cf9469311815acdf
YuraGafurov <113185327+YuraGafurov@users.noreply.github.com>
2022-12-05 05:08:10 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def add(x, y):
+    return x+y
+
+
+def multiply(x, y):
+    return x*y
+
+
+def subtraction(x, y):
+    return x-y
+
+
---
+++ test.py
@@ -0,0 +1,20 @@
+import unittest
+import main
+
+
+class Test(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(main.add(10, 5), 15)
+        self.assertEqual(main.add(3, 5), 8)
+        self.assertEqual(main.add(2, 2), 4)
+
+    def test_multiply(self):
+        self.assertEqual(main.multiply(5, 3), 15)
+        self.assertEqual(main.multiply(2, 3), 6)
+
+    def test_subtraction(self):
+        self.assertEqual(main.subtraction(10, 3), 7)
+        self.assertEqual(main.subtraction(7, 2), 5)
+
+
+
